0d6552c8f0a19f7a59590eab1ff808e246aeb46a
MoritzPatek <57459930+MoritzPatek@users.noreply.github.com>
2020-12-20 22:47:30 +0100

    added first text

---
+++ main.py
@@ -0,0 +1,11 @@
+#so as an absolut beginner when it comes to neural networks i tought i just firstly write my own 
+#very simple one and then work myself to the point i want to be at
+
+#so what is a neural network actually?
+#those words are really sounding more complicated then they actually are
+
+#basically a neurol network consists out of inputs, hidden layers, outputs and the weights (don't accept a fat mom joke now)
+#but she kinda thicc tho 
+
+#the inputs reseave data from what ever, that data can come from sensors or images, it just has to be readable for the machine
+#so its a must to somehow convert the real data to ones and zeros
