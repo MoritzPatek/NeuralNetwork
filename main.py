406113249c37a22f695716016635de664e115770
MoritzPatek <57459930+MoritzPatek@users.noreply.github.com>
2020-12-21 16:23:37 +0100

    little changes

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 #so what is a neural network actually?
 #those words are really sounding more complicated then they actually are
 
-#basically a neurol network consists out of inputs, hidden layers, outputs and the weights (don't accept a fat mom joke now)
+#basically a neural network consists out of inputs, hidden layers, outputs and the weights (don't accept a fat mom joke now)
 #but she kinda thicc tho 
 
 #the inputs reseave data from what ever, that data can come from sensors or images, it just has to be readable for the machine
 #so its a must to somehow convert the real data to ones and zeros
+
